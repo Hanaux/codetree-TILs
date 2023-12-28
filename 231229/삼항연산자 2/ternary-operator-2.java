5f3aed64e1e695d00811780f4248c8fe9a2a30e9
Seungheon Han <80887177+Hanaux@users.noreply.github.com>
2023-12-29 01:08:13 +0900

    Title: 삼항연산자 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231229/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        char result = (a==1) ? 't' : 'f';
+
+        System.out.println(result);
+    }
+}
