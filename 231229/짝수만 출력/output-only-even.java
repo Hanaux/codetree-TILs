9b3f8b48cf20815733287faa6315053a8befb1f1
Seungheon Han <80887177+Hanaux@users.noreply.github.com>
2023-12-29 04:02:27 +0900

    Title: 짝수만 출력, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231229/짝수만 출력/output-only-even.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        String result = "";
+
+        while(a<=b){
+            result += a+" ";
+            a = a+2;
+        }
+        System.out.println(result);
+    }
+}
