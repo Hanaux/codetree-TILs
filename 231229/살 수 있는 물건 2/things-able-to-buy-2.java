d8a0c0a68e89dd0889fbf9d10f0a9cbbbb75cf39
Seungheon Han <80887177+Hanaux@users.noreply.github.com>
2023-12-29 01:18:08 +0900

    Title: 살 수 있는 물건 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231229/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String result;
+
+        if(n<500) result = "no";
+        else if(n<1000) result = "pen";
+        else if(n<3000) result = "mask";
+        else result = "book";
+
+        System.out.println(result);
+    }
+}
