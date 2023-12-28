faf4b3e6ce273ade35caecae9be37068746f629e
Seungheon Han <80887177+Hanaux@users.noreply.github.com>
2023-12-29 01:22:06 +0900

    Title: 두 번의 연산, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231229/두 번의 연산/two-operations.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        if(a%2!=0) a +=3;
+        
+        if(a%3==0) a = a/3;
+
+        System.out.println(a);
+    }
+}
