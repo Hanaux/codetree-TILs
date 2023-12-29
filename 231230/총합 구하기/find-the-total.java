13917af58f95b9bb0560758be1c8f9343931be2a
Seungheon Han <80887177+Hanaux@users.noreply.github.com>
2023-12-30 00:15:59 +0900

    Title: 총합 구하기, Time: 139ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 231230/총합 구하기/find-the-total.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = 0;
+
+        int check = (a/6)*6;
+
+        while(check<=b){
+            if(check>a && check%8 != 0){
+                sum += check;
+            }
+            check += 6;
+        }
+
+        System.out.println(sum);
+    }
+}
