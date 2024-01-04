63322a3b8eab7819dadb6c06246ce0e69b52bc67
Seungheon Han <80887177+Hanaux@users.noreply.github.com>
2024-01-05 04:21:53 +0900

    Title: 구구단으로 이루어진 직각 삼각형, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240105/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        // int pre = 1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                if(j!=1) System.out.print(" / ");
+                System.out.print(i+" * "+j+" = "+i*j);
+            }
+            System.out.println();
+        }
+        
+    }
+}
